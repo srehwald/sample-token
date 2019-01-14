36d9afdc30f583c7c847537bb29fe3c603fd5678
Simon Rehwald <simon.rehwald@tum.de>
2019-01-14 20:24:54 +0100

    + Template for secrets.js

---
+++ secrets.template.js
@@ -0,0 +1,4 @@
+var infuraKey = "TODO: <infura key / project id>";
+var ropstenMnemonic = "TODO: <mnemonic>";
+var mainnetMnemonic = "TODO: <mnemonic>";
+module.exports = {infuraKey: infuraKey, ropstenMnemonic: ropstenMnemonic, mainnetMnemonic: mainnetMnemonic};
